0ae762111b28af2f88596770542f2ceb7f630e73
Sam Tay <sam.chong.tay@gmail.com>
2016-10-07 13:32:43 -0400

    Type constructors and value constructors

--- playground/Record.hs
+++ playground/Record.hs
@@ -13,4 +13,12 @@ data Person = Person { firstName :: String
 
 data Car = Car {company :: String, model :: String, year :: Int} deriving (Show)
 
-data Vector = Vector Int Int Int deriving (Show)
+data Vector a = Vector a a a deriving (Show)
+ -- Note (Vector a) is the type constructor
+    --   while (Vector a a a) is the value constructor
+
+-- Note this type declaration takes the `type constuctor` with its single parameter
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
+-- while the pattern matching is done against the `value constructor` with its three parameters
+
