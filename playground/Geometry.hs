f708dbecc92ebc3bde948763e9f157882d816dac
Sam Tay <sam.chong.tay@gmail.com>
2016-10-07 01:37:35 -0400

    First module

---
+++ playground/Geometry.hs
@@ -0,0 +1,29 @@
+module Geometry
+( sphereVolume
+, sphereArea
+, cubeVolume
+, cubeArea
+, cuboidArea
+, cuboidVolume
+) where
+
+sphereVolume :: Float -> Float
+sphereVolume r = (4.0 / 3.0) * pi * (r^3)
+
+sphereArea :: Float -> Float
+sphereArea r = 4 * pi * (r ^ 2)
+
+cubeVolume :: Float -> Float
+cubeVolume side = cuboidVolume side side side
+
+cubeArea :: Float -> Float
+cubeArea side = cuboidArea side side side
+
+cuboidVolume :: Float -> Float -> Float -> Float
+cuboidVolume a b c = rectangleArea a b * c
+
+cuboidArea :: Float -> Float -> Float -> Float
+cuboidArea a b c = rectangleArea a b * 2 + rectangleArea a c * 2 + rectangleArea c b * 2
+
+rectangleArea :: Float -> Float -> Float
+rectangleArea a b = a * b
