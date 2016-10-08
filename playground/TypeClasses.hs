1491f64873b7ce24acc7d21b1e554651efc08772
Sam Tay <sam.chong.tay@gmail.com>
2016-10-08 15:30:42 -0400

    Playing with type classes

---
+++ playground/TypeClasses.hs
@@ -0,0 +1,21 @@
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+  Red == Red = True
+  Yellow == Yellow = True
+  Green == Green = True
+  _ == _ = False
+
+instance Show TrafficLight where
+  show Red = "Red Light"
+  show Yellow = "Yellow Light"
+  show Green = "Green Light!"
+
+
+{-- standard Prelude Eq class def
+class Eq a where  
+  (==) :: a -> a -> Bool  
+  (/=) :: a -> a -> Bool  
+  x == y = not (x /= y)  
+  x /= y = not (x == y) 
+--}
