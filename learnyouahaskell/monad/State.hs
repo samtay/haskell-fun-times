6d0cddd5ae89c6970066b40a11217882ebd083b0
Sam Tay <sam.chong.tay@gmail.com>
2016-10-23 22:04:59 -0400

    Modify State until it fits with present times

--- learnyouahaskell/monad/State.hs
+++ learnyouahaskell/monad/State.hs
@@ -1,10 +1,9 @@
--- learnyouahaskell is dated -- current State monads look pretty different.
--- @TODO come back in here after other tutorials and bring learnyouahaskell info up to date
 module Stack where
+import Control.Applicative
+import Control.Monad (liftM, ap)
 -- While functions' inability to change global state/variables leads to better programs,
 -- some problems are inherently stateful
 -- This can be tedious in Haskell, but the state monad helps
-import Control.Monad.State
 
 type Stack = [Int]
 
@@ -21,25 +20,55 @@ stackOps' stack = let
     (a ,thirdStack)  = pop' secondStack
     in pop' thirdStack
 
-{-- ^^ A bit tedious. enter the State monad
+-- ^^ A bit tedious. enter the State monad
 
 newtype State s a = State { runState :: s -> (a,s) }
 
+instance Functor (State s) where
+    fmap = liftM
+instance Applicative (State s) where
+    pure = return
+    (<*>) = ap
 instance Monad (State s) where
     return x = State $ \s -> (x,s)
     (State h) >>= f = State $ \s -> let (a, newState) = h s
                                         (State g) = f a
                                     in g newState
---}
-pop :: StateT Stack Int
-pop = StateT $ \(x:xs) -> (x,xs)
+pop :: State Stack Int
+pop = State $ \(x:xs) -> (x,xs)
 
-push :: Int -> StateT Stack ()
-push a = StateT $ \xs -> ((),a:xs)
+push :: Int -> State Stack ()
+push a = State $ \xs -> ((),a:xs)
 
 -- Note, we are NOT passing stateful stack as an argument -- thats what we wanted to avoid, huzzah
-stackOps :: StateT Stack Int
+stackOps :: State Stack Int
 stackOps = do
     push 3
-    a <- pop
     pop
+    pop
+
+stackStuff :: State Stack ()
+stackStuff = do
+    a <- pop
+    if a == 5 then push 5 else do
+        push 3
+        push 8
+
+combinedOps :: State Stack ()
+combinedOps = do
+    a <- stackOps
+    if a == 100
+       then stackStuff
+       else return ()
+
+-- MonadState implements get and put as below
+
+get = State $ \s -> (s,s)
+put newState = State $ \s -> ((), newState)
+
+stackyStack :: State Stack ()
+stackyStack = do
+    stackNow <- get
+    if stackNow == [1,2,3]
+       then put [8,3,1]
+       else put [9,2,1]
