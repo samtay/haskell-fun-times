32c997c7f1cd290e32b0700a8bfb251962e1c98d
Sam Tay <sam.chong.tay@gmail.com>
2016-10-30 21:01:25 -0400

    Update comments on `Pair` after recent eurekas

--- typeclassopedia/3_Functor/3.2_Instances.hs
+++ typeclassopedia/3_Functor/3.2_Instances.hs
@@ -19,13 +19,10 @@ instance Functor ((,) e) where
 instance Functor Pair where
   fmap g (Pair x y) = Pair (g x) (g y)
 
--- While these look similar, the difference lies in the kind of the types. Both
--- are type constructors that accept a single concrete type, however ((,) e) has
+-- While these look similar, the difference lies in the values held by each type. Both
+-- are type constructors that accept a single concrete type, however ((,) e) holds
 -- a predefined type (that of e) that is independent of the additional type parameter,
 -- while the "first" data constructor parameter of Pair depends on the type parameter.
--- Because of this, I chose to fmap across both elements in the Pair, which makes this
--- instance less useful than ((,) e), since we're essentially just duplicating content.
--- TODO: Make sure we shouldn't implement this as Pair x (g y)
 
 
 -- 3. Implement a Functor instance for the type ITree defined as
