3fa829e04612072f0fd9cd8ab2699bc553a15c95
Sam Tay <sam.chong.tay@gmail.com>
2016-11-13 02:06:21 -0500

    Fix `splitBy` func to respect splitter arg

--- from-first-principles/9_Lists/Better.hs
+++ from-first-principles/9_Lists/Better.hs
@@ -4,6 +4,6 @@ module Better where
 splitBy :: Char -> String -> [String]
 splitBy c cs = reverse $ go [] (dropWhile (==c) cs)
   where go ws "" = ws
-        go ws s  = let next  = takeWhile (/='\n') s
-                       rest = (dropWhile (=='\n')) . (dropWhile (/='\n')) $ s
+        go ws s  = let next  = takeWhile (/=c) s
+                       rest = (dropWhile (==c)) . (dropWhile (/=c)) $ s
                        in go (next:ws) rest
