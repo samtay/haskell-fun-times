ecd09dec9d43e453831e3e9fc35d6bab651e322d
Sam Tay <sam.chong.tay@gmail.com>
2016-11-28 00:39:42 -0500

    Fix capitalize paragraph func

--- from-first-principles/11_Algebraic-Datatypes/LanguageExercises.hs
+++ from-first-principles/11_Algebraic-Datatypes/LanguageExercises.hs
@@ -5,10 +5,10 @@ import Data.Char
 -- 1. Write a function that capitalizes a word.
 capitalizeWord :: String -> String
 capitalizeWord "" = ""
-capitalizeWord (c:cs) = (toUpper c):cs
+capitalizeWord (c:cs) = toUpper c : cs
 
 -- 2. Write a function that capitalizes sentences in a paragraph. Recognize when a new sentence has begun by checking for periods.  Reuse the capitalizeWord function.
 capitalizeParagraph :: String -> String
-capitalizeParagraph s = (unwords . go . words) (capitalizeWord s)
-  where go (x:y:xs) = if last x == '.' then x:(capitalizeWord y):(go xs) else x:y:(go xs)
-        go xs = xs
+capitalizeParagraph s = (unwords . cp . words) (capitalizeWord s)
+  where cp (x:y:xs) = if last x == '.' then x : capitalizeWord y : cp xs else x : cp (y:xs)
+        cp xs = xs
