47123cae0d4e4e4dcf67881f900714e742c5848d
Sam Tay <sam.chong.tay@gmail.com>
2016-12-22 21:17:07 -0500

    Some Applicative instances

---
+++ from-first-principles/17_Applicative/IdentityConstant.hs
@@ -0,0 +1,22 @@
+module IdentityConstant where
+
+newtype Identity a = Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
+
+instance Applicative Identity where
+  pure = Identity
+  (Identity f) <*> (Identity x) = Identity (f x)
+
+newtype Constant a b =
+  Constant { getConstant :: a }
+  deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant a) = Constant a
+
+instance Monoid a => Applicative (Constant a) where
+  pure _ = Constant mempty
+  (Constant x) <*> (Constant y) = Constant (x `mappend` y)
