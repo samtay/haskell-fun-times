af9e954518823185edacb034f6fed884ac381fb5
Sam Tay <sam.chong.tay@gmail.com>
2017-01-22 01:22:47 -0500

    Safety in gh-pages generator

--- build-github-pages.sh
+++ build-github-pages.sh
@@ -1,36 +1,32 @@
 #!/usr/bin/env bash
 
-# usage: __build topdir
-__build() {
-  local topdir=$1
-
+# usage: build
+build() {
   ## compile .hs files to .md files
-  __compileHs $topdir
+  compileHs
 
   ## aggregate all .md files
   local content=""
   local sep=$'\n\n'
-  for mdFile in $(find -type f -name "*.md" -path "./$topdir*" | sort); do
+  for mdFile in $(find -type f -name "*.md" -path "./$__topdir*" | sort); do
     # poor mans sorting
-    if [[ "$mdFile" == "./$topdir/$(basename $mdFile)" ]]; then
+    if [[ "$mdFile" == "./$__topdir/$(basename $mdFile)" ]]; then
       content="$(cat $mdFile)$sep$content"
     else
       content="$content$sep$(cat $mdFile)"
     fi
   done
 
-  # for some reason this doesn't work: pandoc -s -o "$topdir.html" "$mdFiles"
-  rm "$topdir.html"
-  echo "$content" | pandoc --read=markdown_github --standalone --output "$topdir.html"
+  # for some reason this doesn't work: pandoc -s -o "$__topdir.html" "$mdFiles"
+  rm -f "$__topdir.html"
+  echo "$content" | pandoc --read=markdown_github --standalone --output "$__topdir.html"
 }
 
 # compiles haskell files
 # cp *.hs -> *.md and add gfm syntax hints
-# usage: __compileHs topdir
-__compileHs() {
-  local topdir=$1
-
-  for hsfile in $(find -type f -path "./$topdir*.hs" -not -path "*stack-work*" | sort); do
+# usage: compileHs
+compileHs() {
+  for hsfile in $(find -type f -path "./$__topdir*.hs" -not -path "*stack-work*" | sort); do
     cp $hsfile "$hsfile.md"
     sed -i '1i ```haskell' "$hsfile.md"
     echo '```' >> "$hsfile.md"
@@ -38,7 +34,7 @@ __compileHs() {
 }
 
 # cleanup *.hs.md files, git stuff
-__cleanup() {
+cleanup() {
   for f in $(find -type f -name "*.hs.md"); do
     rm $f
   done
@@ -46,6 +42,7 @@ __cleanup() {
 }
 
 main() {
+  set -eo pipefail
   local -a topdirs=(from-first-principles learn-you-a-haskell cis-194 typeclassopedia)
 
   # sync with master
@@ -53,20 +50,21 @@ main() {
   git reset --hard master
 
   # build HTML content
-  for topdir in ${topdirs[@]}; do
-    __build $topdir
-    sed -i -e "s/.\/$topdir/$topdir.html/g" README.md
+  for __topdir in ${topdirs[@]}; do
+    build
+    sed -i -e "s/.\/$__topdir/$__topdir.html/g" README.md
   done
   pandoc -s -o "index.html" README.md
 
   # send off to github
-  for f in ${topdirs[@]} + "index"; do
+  for f in "${topdirs[@]}"; do
     git add "$f.html"
   done
+  git add "index.html"
   git commit -m 'Compiled haskell content into somewhat readable html'
   git push -f
 
-  __cleanup
+  cleanup
   git checkout master
 }
 
