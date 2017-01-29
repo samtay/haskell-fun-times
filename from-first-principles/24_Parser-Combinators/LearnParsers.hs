bfd212d74943d3c6f4d3dfa8cdceebb899c2b2d2
Sam Tay <sam.chong.tay@gmail.com>
2017-01-29 01:17:43 -0500

    Learning about parsing

--- from-first-principles/24_Parser-Combinators/LearnParsers.hs
+++ from-first-principles/24_Parser-Combinators/LearnParsers.hs
@@ -1,21 +1,57 @@
 module LearnParsers where
 
 import Text.Trifecta
+import Text.Parser.LookAhead
+import Control.Applicative ((<|>))
 
-one :: CharParsing m => m Char
 one = char '1'
 
-stop :: Parser a
 stop = unexpected "stop"
 
 one' = one >> stop
 
 -- read two characters, '1', and '2'
 oneTwo = char '1' >> char '2'
+
 -- read two characters, '1' and '2', then die
 oneTwo' = oneTwo >> stop
 
-testParse :: Parser Char -> IO ()
+-- Exercises: Parsing Practice
+
+-- 1. Use `eof` from Text.Parser.Combinator to make the `one` and `oneTwo` parsers fail
+-- for not exhausting the input stream.
+ex1_one :: Parser ()
+ex1_one = one >> eof
+
+ex1_oneTwo :: Parser ()
+ex1_oneTwo = oneTwo >> eof
+
+ex1_oneTwoThree :: Parser ()
+ex1_oneTwoThree = oneTwo >> char '3' >> eof
+
+-- 2. Use `string` to make a Parser that parses "1", "12", and "123" out of the example input.
+-- Try combining it with `stop` too.
+-- That is, a single parser should be able to parse all three of those strings.
+
+ex2 :: Parser [String]
+-- these all look for "112123"
+-- ex2 = traverse string ["1", "12", "123"]
+-- ex2 = traverse (try . string) ["1", "12", "123"]
+-- ex2 = sequenceA $ string <$> ["1", "12", "123"]
+-- goddamn finally
+-- ex2 = do
+--   r1 <- lookAhead $ string "1"
+--   r2 <- lookAhead $ string "12"
+--   r3 <- lookAhead $ string "123"
+--   return [r1, r2, r3]
+--  cleanup
+ex2 = traverse (lookAhead . string) ["1", "12", "123"]
+
+-- 3. Try writing a Parser that does what `string` does, but using `char`.
+ex3 :: CharParsing m => String -> m String
+ex3 = traverse char
+
+testParse :: Show a => Parser a -> IO ()
 testParse p =
   print $ parseString p mempty "123"
 
@@ -23,12 +59,20 @@ pNL s = putStrLn ('\n' : s)
 
 main = do
   pNL "stop:"
-  testParse stop
+  testParse (stop :: Parser Char)
   pNL "one:"
   testParse one
   pNL "one':"
-  testParse one'
+  testParse (one' :: Parser Char)
   pNL "oneTwo:"
   testParse oneTwo
   pNL "oneTwo':"
-  testParse oneTwo'
+  testParse (oneTwo' :: Parser Char)
+  pNL "ex1_one:"
+  testParse ex1_one
+  pNL "ex1_oneTwo:"
+  testParse ex1_oneTwo
+  pNL "ex1_oneTwoThree:"
+  testParse ex1_oneTwoThree
+  pNL "ex2:"
+  testParse ex2
--- from-first-principles/24_Parser-Combinators/README.md
+++ from-first-principles/24_Parser-Combinators/README.md
@@ -57,3 +57,47 @@ The idea is:
 Check out some [demonstrations](./LearningParsers.hs).
 
 #### Exercises Parsing Practice
+1. Use `eof` from Text.Parser.Combinator to make the `one` and `oneTwo` parsers fail
+for not exhausting the input stream.
+
+  ```haskell
+  -- Exercises: Parsing Practice
+
+  ex1_one :: Parser ()
+  ex1_one = one >> eof
+
+  ex1_oneTwo :: Parser ()
+  ex1_oneTwo = oneTwo >> eof
+
+  ex1_oneTwoThree :: Parser ()
+  ex1_oneTwoThree = oneTwo >> char '3' >> eof
+  ```
+
+2. Use `string` to make a Parser that parses "1", "12", and "123" out of the example input.
+Try combining it with `stop` too.
+That is, a single parser should be able to parse all three of those strings.
+
+  ```haskell
+  ex2 :: Parser [String]
+  -- these all look for "112123"
+  -- ex2 = traverse string ["1", "12", "123"]
+  -- ex2 = traverse (try . string) ["1", "12", "123"]
+  -- ex2 = sequenceA $ string <$> ["1", "12", "123"]
+  -- goddamn finally
+  -- ex2 = do
+  --   r1 <- lookAhead $ string "1"
+  --   r2 <- lookAhead $ string "12"
+  --   r3 <- lookAhead $ string "123"
+  --   return [r1, r2, r3]
+  --  cleanup
+  ex2 = traverse (lookAhead . string) ["1", "12", "123"]
+  ```
+
+3. Try writing a Parser that does what `string` does, but using `char`.
+
+  ```haskell
+  ex3 :: CharParsing m => String -> m String
+  ex3 = traverse char
+  ```
+
+### 24.4 Parsing fractions
