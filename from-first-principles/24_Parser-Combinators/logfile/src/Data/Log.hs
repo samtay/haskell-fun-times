80d3f21438904cc014fe8af2d44020a5f8f3ccbd
Sam Tay <sam.chong.tay@gmail.com>
2017-02-03 01:22:18 -0500

    Parsing log file
    Preliminary tests passing. Still need to
    
    - modify ignoring comments on same line as activities
    - implement bidrectional show <-> parse
    - validate via quickcheck generators.

--- from-first-principles/24_Parser-Combinators/logfile/src/Data/Log.hs
+++ from-first-principles/24_Parser-Combinators/logfile/src/Data/Log.hs
@@ -2,6 +2,7 @@ module Data.Log where
 
 import Text.Trifecta
 import Control.Applicative ((<|>))
+import Data.Time
 
 {-
 -- 5 --
@@ -12,3 +13,91 @@ The format supports the use of comments which your parser
 will have to ignore. The # characters followed by a date mark
 the beginning of a particular day.
 -}
+
+-- Types
+
+-- | Alias for log entry activity description
+type Activity = String
+type LogS = [LogEntryS]
+type LogE = [LogEntryE]
+
+-- | Log entry consists of start time + activity
+data LogEntryS =
+  LogEntryS UTCTime Activity
+  deriving (Eq, Ord, Show)
+
+-- | Log entry consists of elapsed time + activity
+data LogEntryE =
+  LogEntryE DiffTime Activity
+  deriving (Eq, Ord, Show)
+
+-- | Convert a log with start times to a log with elapsed times
+--
+-- This is a uni-directional conversion, using diffs between start times.
+-- Last log entry will have 0 elapsed time
+startToElapsed :: LogS -> LogE
+startToElapsed = undefined
+
+-- | Get list of activities & average time spent per day
+--
+-- If log spans 5 days, and Rocket League was played 5 hours the first day,
+-- and 10 hours the last day, it would have an entry with average 3 hours.
+avgPerActivity :: LogE -> [(Activity, DiffTime)]
+avgPerActivity = undefined
+
+-- Parsers
+
+parseLog :: Parser LogS
+parseLog = do
+  skipComments
+  entries <- some $ do
+    string "# "
+    d  <- parseDay
+    skipComments
+    ls <- some $ do
+      l <- parseLogEntry
+      skipComments
+      return l
+    skipComments
+    return $ map (\(t, a) -> LogEntryS (UTCTime d t) a) ls
+  return $ concat entries
+
+parseLogEntry :: Parser (DiffTime, Activity)
+parseLogEntry = do
+  t <- parseDTime
+  char ' '
+  a <- some (notChar '\n')
+  newline
+  return (t, a)
+
+parseDTime :: Parser DiffTime
+parseDTime = do
+  hr <- digits 2
+  char ':'
+  min <- digits 2
+  return $ secondsToDiffTime $ 3600 * hr + 60 * min
+
+digits :: (Num a, Read a) => Int -> Parser a
+digits n = read <$> count n digit
+
+parseDay :: Parser Day
+parseDay = do
+  yr <- integer
+  char '-'
+  mnth <- digits 2
+  char '-'
+  day <- digits 2
+  return $ fromGregorian yr mnth day
+
+skipComments :: Parser ()
+skipComments = do
+  spaces
+  skipMany parseComment
+  spaces
+
+parseComment :: Parser ()
+parseComment = do
+  string "--"
+  skipMany (notChar '\n')
+  newline
+  return ()
--- from-first-principles/24_Parser-Combinators/logfile/test/Spec.hs
+++ from-first-principles/24_Parser-Combinators/logfile/test/Spec.hs
@@ -1,15 +1,35 @@
 module Main where
 
 import Test.QuickCheck hiding (Result(..))
+import Test.Hspec
 import Text.Trifecta
+import Data.Log
+import Data.Maybe (isJust)
+import Data.Time
 
 -- TODO quickcheck generators
 
--- This is done because Result doesn't have Show instance
-toMaybe :: Result a -> Maybe a
-toMaybe (Success x) = Just x
-toMaybe _           = Nothing
--- Runtime
-
 main :: IO ()
-main = undefined
+main = do
+  hspec specs
+  -- some quickcheck stuff here
+
+specs :: Spec
+specs =
+  describe "Parsing log file" $ do
+    r <- runIO $ parseFromFile parseLog "test/fixtures/chrisdiary.txt"
+
+    it "can parse Chris's diary example" $
+      r `shouldSatisfy` isJust
+
+    it "parsed first entry accurately" $
+      head <$> r
+        `shouldBe` Just (LogEntryS (UTCTime (fromGregorian 2025 2 5) (8*3600)) "Breakfast")
+
+    it "parsed last entry accurately" $
+      last <$> r
+        `shouldBe` Just (LogEntryS (UTCTime (fromGregorian 2025 2 7) (22*3600)) "Sleep")
+
+    it "ignores comments in activities" $
+      (!!11) <$> r
+        `shouldBe` Just (LogEntryS (UTCTime (fromGregorian 2025 2 7) (8*3600)) "Breakfast")
