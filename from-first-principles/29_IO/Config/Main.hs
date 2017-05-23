47ed02889a159053e1934a600f56e3dac65cc0b4
Sam Tay <sam.chong.tay@gmail.com>
2017-05-22 21:13:42 -0400

    Exercise parsing ini files, finish ch 29

--- from-first-principles/29_IO/Config/Main.hs
+++ from-first-principles/29_IO/Config/Main.hs
@@ -17,25 +17,32 @@ module Main where
 --Again, after writing Dockmaster, I could do this quickly via shelly
 --but will learn more by restricting myself to base.
 
-import Data.Map (Map)
+import Text.Trifecta (Result(..), parseFromFileEx)
 
-import Control.Monad ((>=>))
+import Data.List (isSuffixOf)
+import Data.Map (Map, fromList, toList)
+import Control.Monad ((>=>), (<=<), void)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
+import System.Directory (listDirectory)
 import System.IO (FilePath, stderr, hPutStrLn)
 
-import Data.Ini hiding (main)
+import Data.Ini (Config, parseIni)
 
 main :: IO ()
 main =
   (parseArgs <$> getArgs)
     >>= maybe argError (parseFilesInDir >=> showResult)
 
-parseFilesInDir :: FilePath -> IO (Map FilePath (Maybe Config))
-parseFilesInDir = undefined
+parseFilesInDir :: FilePath -> IO (Map FilePath (Result Config))
+parseFilesInDir d = fmap fromList . traverse parse =<< (fmap onlyIni . listDirectory $ d)
+  where onlyIni = filter (isSuffixOf ".ini")
+        parse f = sequence (f, parseFromFileEx parseIni (d ++ "/" ++ f))
 
-showResult :: Map FilePath (Maybe Config) -> IO ()
-showResult = undefined
+showResult :: Map FilePath (Result Config) -> IO ()
+showResult = mapM_ fn . toList
+  where fn (f, (Failure _)) = putStrLn $ f ++ ": Failed to parse\n"
+        fn (f, (Success c)) = putStrLn $ f ++ ": " ++ show c ++ "\n"
 
 argError :: IO ()
 argError =
