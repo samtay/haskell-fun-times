ef297deede5ef443346e3cc974e1bd9e4325b412
Sam Tay <sam.chong.tay@gmail.com>
2017-05-27 23:04:09 -0400

    FFP ch 31 exercise 3

--- from-first-principles/31_Final-Project/app/AddUser.hs
+++ from-first-principles/31_Final-Project/app/AddUser.hs
@@ -11,24 +11,66 @@ import Text.RawString.QQ
 import Database.SQLite.Simple
 import Database.SQLite.Simple.Types
 
+data ExecType = Insert | Update
+
 insertUser :: Connection -> UserRow -> IO ()
 insertUser conn = execute conn insertUserQuery
+
+updateUser :: Connection -> UserRow -> IO ()
+updateUser conn (_, username, shell, homeDirectory, realName, phone) =
+  executeNamed conn updateUserQuery
+    [ ":username" := username
+    , ":homeDirectory" := homeDirectory
+    , ":realName" := realName
+    , ":phone" := phone
+    , ":shell" := shell ]
+
+updateUserQuery :: Query
+updateUserQuery = [r|
+UPDATE users
+SET
+  shell = :shell,
+  homeDirectory = :homeDirectory,
+  realName = :realName,
+  phone = :phone
+WHERE username = :username
+|]
+
+pastTenseAction :: ExecType -> String
+pastTenseAction Insert = "added"
+pastTenseAction Update = "updated"
+
+execAction :: ExecType -> Connection -> UserRow -> IO ()
+execAction Insert = insertUser
+execAction Update = updateUser
 
 main :: IO ()
 main = do
   args <- (fmap . fmap) T.pack getArgs
   case args of
-    [username, shell, homeDir, realName, phone] -> do
-      conn <- open "finger.db"
-      insertUser conn (Null, username, shell, homeDir, realName, phone)
-      putStrLn $ "User " ++ T.unpack username ++ " added successfully."
+    [username, shell, homeDir, realName, phone] ->
+      insert Insert (Null, username, shell, homeDir, realName, phone)
+    ["--update", username, shell, homeDir, realName, phone] ->
+      insert Update (Null, username, shell, homeDir, realName, phone)
+    ["-u", username, shell, homeDir, realName, phone] ->
+      insert Update (Null, username, shell, homeDir, realName, phone)
     _ -> argsError
+    where
+      insert method userRow@(_, username, _, _, _, _) = do
+        conn <- open "finger.db"
+        execAction method conn userRow
+        close conn
+        putStrLn
+          $ unwords ["User", T.unpack username, pastTenseAction method, "successfully."]
 
 argsError :: IO ()
 argsError = hPutStrLn stderr helpinfo >> exitFailure
   where helpinfo = [r|
-Usage: adduser USERNAME SHELL HOME_DIR REALNAME PHONE
+Usage: adduser [OPTION] USERNAME SHELL HOME_DIR REALNAME PHONE
 Adds a user to the finger.db database
 Example: adduser dexmaster3 /bin/bash /home/dexcaff "Dex Caff" 911
+
+Options:
+  -u, --update      Updates existing username
 |]
 
